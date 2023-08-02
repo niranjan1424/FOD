20bf03ae1e573280e221587e86ef5eea8f18b4ab
niranjan <106364901+niranjan1424@users.noreply.github.com>
2023-08-02 14:33:46 +0530

    Create 11.salesline bar scatter plot.py

---
+++ 11.salesline bar scatter plot.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+months = ['January', 'February', 'March', 'April', 'May', 'June']
+sales = [10000, 12000, 8000, 15000, 18000, 13000]
+def create_line_plot(months, sales):
+    plt.plot(months, sales, marker='o', linestyle='-')
+    plt.xlabel('Month')
+    plt.ylabel('Sales')
+    plt.title('Monthly sales data LINE PLOT')
+    plt.grid(True)
+    plt.show()
+create_line_plot(months,sales)
+
+def create_bar_plot(months, sales):
+    colors =['#33FF57']
+    plt.bar(months, sales,color=colors)
+    plt.xlabel('Month')
+    plt.ylabel('Sales')
+    plt.title('Monthly Sales Data BAR PLOT')
+    plt.grid(True)
+    plt.show()
+create_bar_plot(months,sales)
+
+def create_scatter_plot(months, sales):
+    plt.scatter(months, sales)
+    plt.xlabel('Month')
+    plt.ylabel('Sales')
+    plt.title('Monthly Sales Data SCATTER PLOT')
+    plt.grid(True)
+    plt.show()
+create_scatter_plot(months,sales)
